2c11152b3250480dadfdc70a896ff0294bbcdb7e
Jozef Číž <jozef.ciz.ml@gmail.com>
2023-10-29 14:11:50 +0100

    Matfyz

--- content/contest/template.cpp
+++ content/contest/template.cpp
@@ -1,12 +1,28 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-#define rep(i, a, b) for(int i = a; i < (b); ++i)
+#define For(i, lo, hi) for(ll i = lo; i < (ll)(hi); ++i)
 #define all(x) begin(x), end(x)
-#define sz(x) (int)(x).size()
-typedef long long ll;
-typedef pair<int, int> pii;
-typedef vector<int> vi;
+#define vec vector
+using ll = long long;
+const ll INF = (ll)1.1e18 - 1;
+
+template<typename A, typename B>
+ostream& operator<<(ostream& os, const pair<A, B>& p) {
+	return os << '(' << p.first << ", " << p.second << ')';
+}
+template <typename A, typename B, typename C>
+basic_ostream<A, B>& operator<<(basic_ostream<A, B>& os, const C& c) {
+	for (auto it = begin(c); it != end(c); ++it)
+		os << (it == begin(c) ? "" : " ") << *it;
+	return os;
+}
+template<typename T>
+ostream& operator<<(ostream& os, const vector<vector<T>>& c) {
+	for (auto it = begin(c); it != end(c); ++it)
+		os << (it == begin(c) ? "" : "\n") << *it;
+	return os;
+}
 
 int main() {
 	cin.tie(0)->sync_with_stdio(0);
