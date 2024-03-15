488f5b612589948d7be7200fa44c8537d680703d
Jozef Číž <jozef.ciz.ml@gmail.com>
2024-03-15 16:37:26 +0100

    Improved template and bashrc

--- content/contest/.bashrc
+++ content/contest/.bashrc
@@ -1,8 +1,8 @@
-export CXXFLAGS="-O2 -g -std=c++17 -Wall -Wextra -Wconversion \
-	-Wfatal-errors -fsanitize=undefined,address"
-alias c='g++ $CXXFLAGS'
+#!/bin/bash
+alias c="g++ -O2 -g -std=c++17 -Wall -Wextra -Wfatal-errors"
+export CXXFLAGS="-O2 -g -std=c++17 -Wall -Wextra -Wfatal-errors -Wconversion  -fsanitize=undefined,address"
 function cr() {
 	exe=${1%.*}
-	make $exe && ./$exe
+	make $exe && echo compiled && time ./$exe
 }
 xmodmap -e 'clear lock' -e 'keycode 66=less greater' #caps = <>
--- content/contest/template.cpp
+++ content/contest/template.cpp
@@ -1,31 +1,32 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-#define For(i, lo, hi) for(ll i = lo; i < (ll)(hi); ++i)
-#define sz(x) ((ll)size(x))
+// #define MANY_TESTS ll TC; cin >> TC; while (TC--)
+#define For(i, lo, hi) for (ll i = lo; i < (ll)(hi); ++i)
 #define all(x) begin(x), end(x)
+#define sz(x) ((ll)size(x))
 #define vec vector
 using ll = long long;
-const ll INF = (ll)1.1e18 - 1;
+using pll = pair<ll, ll>;
+constexpr ll INF = (ll)1.1e18 - 1;
 
-template<typename A, typename B>
-ostream& operator<<(ostream& os, const pair<A, B>& p) {
-	return os << '(' << p.first << ", " << p.second << ')';
+template <class A, class B>
+ostream& operator<<(ostream& os, const pair<A, B>& c) {
+  return os << "(" << c.first << ", " << c.second << ")";
 }
-template <typename A, typename B, typename C>
+template <class A, class B, class C>
 basic_ostream<A, B>& operator<<(basic_ostream<A, B>& os, const C& c) {
-	for (auto it = begin(c); it != end(c); ++it)
-		os << (it == begin(c) ? "" : " ") << *it;
-	return os;
+  for (auto it = begin(c); it != end(c); ++it)
+    os << (it == begin(c) ? "" : " ") << *it;
+  return os;
 }
-template<typename T>
-ostream& operator<<(ostream& os, const vector<vector<T>>& c) {
-	for (auto it = begin(c); it != end(c); ++it)
-		os << (it == begin(c) ? "" : "\n") << *it;
-	return os;
+template <class... Args>
+void dbg(Args&& ...args) {
+  ((cerr << args << " | "), ...);
+  cerr << endl;
 }
 
 int main() {
-	cin.tie(0)->sync_with_stdio(0);
-	cin.exceptions(cin.failbit);
+  cin.tie(0)->sync_with_stdio(0);
+  cin.exceptions(cin.failbit);
 }
