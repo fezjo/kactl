77471c25d25ee81897a7e5b01239f9328cc02d32
Jozef Číž <jozef.ciz.ml@gmail.com>
2024-10-20 20:46:38 +0200

    Update template, shrink equation

--- content/contest/template.cpp
+++ content/contest/template.cpp
@@ -1,14 +1,13 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-// #define MANY_TESTS ll TC; cin >> TC; while (TC--)
 #define For(i, lo, hi) for (ll i = lo; i < (ll)(hi); ++i)
 #define all(x) begin(x), end(x)
 #define sz(x) ((ll)size(x))
 #define vec vector
 using ll = long long;
 using pll = pair<ll, ll>;
-constexpr ll INF = (ll)1.1e18 - 1;
+constexpr ll INF = (ll)1.001e18 - 1;
 
 template <class A, class B>
 ostream& operator<<(ostream& os, const pair<A, B>& c) {
@@ -20,13 +19,15 @@ basic_ostream<A, B>& operator<<(basic_ostream<A, B>& os, const C& c) {
     os << (it == begin(c) ? "" : " ") << *it;
   return os;
 }
-template <class... Args>
-void dbg(Args&& ...args) {
-  ((cerr << args << " | "), ...);
-  cerr << endl;
+template<class...A> void dbg(A&&...a) {
+  ((cerr << a << " | "), ...) << endl;
 }
 
+void solve() {}
 int main() {
   cin.tie(0)->sync_with_stdio(0);
   cin.exceptions(cin.failbit);
+  ll TC = 1;
+  // cin >> TC;
+  For(i, 0, TC) solve();
 }
